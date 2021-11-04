08f4bb94ffc5920ca6a32ccbca46e600f30b2a93
KAUA MACAHADO FERREIRA <kaua.macahado@alunoescolassociais.org.br>
2021-11-04 17:32:06 -0300

    Formulário / Arquivos

---
+++ _js/controlle.js
@@ -0,0 +1,15 @@
+function verificar(){
+	var nome = document.getElementById('nome').value;
+	var sobrenome = document.getElementById('nome').value;
+	var nome = document.getElementById('nome').value;
+	var nome = document.getElementById('nome').value;
+	var nome = document.getElementById('nome').value;
+	var nome = document.getElementById('nome').value;
+	var nome = document.getElementById('nome').value;
+	var nome = document.getElementById('nome').value;
+
+	if (!nome | | !sobrenome){
+		 alert("Campo não preenchidos , favor preenchê-los");
+	}  else{
+		alert("Campos preenchidos com sucesso");
+	}	
